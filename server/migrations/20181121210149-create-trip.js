345b5bd2b32728780d01c6e3d8e99208e91153a1
Dario Elbadry <elbadry.d@gmail.com>
2018-11-22 14:12:03 -0600

    updated migration file

--- server/migrations/20181121210149-create-trip.js
+++ server/migrations/20181121210149-create-trip.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Trips', {
+    return queryInterface.createTable('trips', {
       id: {
         allowNull: false,
         autoIncrement: true,

33f2f3fdf214c2df9d9caaaebadf2e8c84169f3c
Randy Calcaterra <opalibis64@gmail.com>
2018-11-21 20:43:40 -0600

    working on autho

---
+++ pages/forms/login.js
@@ -0,0 +1,11 @@
+
+const Login = () => (
+    <div>
+        Username
+        <input></input>
+        Password
+        <input></input>
+    </div>
+)
+
+export default Login;
--- server/ssr-server.js
+++ server/ssr-server.js
@@ -7,17 +7,30 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+
+
+
+
 app.prepare()
   .then(() => {
     const server = express();
     server.get('/test', (req, res) => {
       res.send('stillgot it');
     });
+
+    server.get('/', (req, res) => {
+      res.send('bazinga');
+    });
+
     server.get('*', (req, res) => handle(req, res));
     server.listen(3000, (err) => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
+
+    // server.get('/forms/login', (req, res) => {
+    //   res.send('this actually worked');
+    // });
   })
   .catch((ex) => {
     console.error(ex.stack);

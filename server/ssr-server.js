d9441eb538c434af81a8942f0c1b7785c3b57577
Dario Elbadry <elbadry.d@gmail.com>
2018-11-23 02:43:57 -0600

    added images to pop ups, zoom in on load

--- server/ssr-server.js
+++ server/ssr-server.js
@@ -12,12 +12,14 @@ app.prepare()
   .then(() => {
     const server = express();
     server.get('/createRoute', (req, res) => {
+      console.log(req.query);
       const origin = JSON.parse(req.query.originCoords);
       const dest = JSON.parse(req.query.destCoords);
       const start = `${origin.lng},${origin.lat}`;
       const end = `${dest.lng},${dest.lat}`;
       console.log(end, start);
       helpers.makeTrip(start, end, 'context', (a, obj) => {
+        console.log(obj);
         res.send(obj);
       });
     });

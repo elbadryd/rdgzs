a1da2fb738d1ca67ef0ef4c537d30f00dd1cb7db
Randy Calcaterra <opalibis64@gmail.com>
2018-11-23 16:03:41 -0600

    signup checks users and adds to db if email is not in db, needs bcrypt. login needs debugging but passport works; issue with succes/failure redirects on server.post('/login') handler; issue with passport.deseralizeUser looping non stop, check server console logs after successful login to observe, currently line 78 in ssr-server.js

--- pages/forms/login.js
+++ pages/forms/login.js
@@ -1,11 +1,60 @@
+import axios from 'axios';
 
-const Login = () => (
-    <div>
-        Username
-        <input></input>
-        Password
-        <input></input>
-    </div>
-)
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        email: null,
+        password: null,
+        // username: null,
+    };
+    this.signUp = this.signUp.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.submitLogin = this.submitLogin.bind(this);
+  }
+
+  handleChange() {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  signUp() {
+    axios.post('/signup', {
+      email: this.state.email,
+      password: this.state.password,
+    }).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  submitLogin() {
+    axios.post('/login', {
+      email: this.state.email,
+      password: this.state.password,
+    }).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <span>Email:</span>
+        <input name="email" onChange={this.handleChange}></input>
+        <span>Password:</span>
+        <input name="password" onChange={this.handleChange}></input>
+        <button onClick={this.submitLogin}>Submit</button>
+        <button onClick={this.signUp}>SignUp</button>
+      </div>
+    )
+  }
+}
+
 
 export default Login;

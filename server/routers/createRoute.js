5fa8d91b22e99f7354a3c6bc38b761ba7c4926cf
J Patrick <jpatricknola@gmail.com>
2018-11-26 17:02:38 -0600

    sending updated geoline to map component after stop added

--- server/routers/createRoute.js
+++ server/routers/createRoute.js
@@ -4,14 +4,13 @@ const helpers = require('../helpers');
 const createRoute = express.Router();
 
 createRoute.get('/', (req, res) => {
-  console.log(req.query, 'in createRoute router');
+  // console.log(req.query, 'in createRoute router');
   const origin = JSON.parse(req.query.originCoords);
   const dest = JSON.parse(req.query.destCoords);
   const start = `${origin.lng},${origin.lat}`;
   const end = `${dest.lng},${dest.lat}`;
   console.log(end, start);
   helpers.makeTrip(start, end, 'context', (a, obj) => {
-    console.log(obj, 'helper');
     res.send(obj);
   });
 });

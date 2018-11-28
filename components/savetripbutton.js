b70a2be52b07c95f658f6350cc66fcbe92f7e8cc
Randy Calcaterra <opalibis64@gmail.com>
2018-11-28 15:19:36 -0600

    may be abandoned

---
+++ components/savetripbutton.js
@@ -0,0 +1,9 @@
+
+
+  const SaveTripButton = () => (
+    <button type='button' class='btn btn-info'>
+      <img src='static/small_save.png'></img>
+    </button>
+)
+
+export default SaveTripButton;

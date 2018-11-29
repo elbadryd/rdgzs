797428f9f0dd1245436eda659ea1c04c7b787d0d
Dario El-badry <elbadry.d@gmail.com>
2018-11-29 12:04:49 -0600

    Delete savetripbutton.js

--- components/savetripbutton.js
+++
@@ -1,9 +0,0 @@
-
-
-  const SaveTripButton = () => (
-    <button type='button' class='btn btn-info'>
-      <img src='static/small_save.png'></img>
-    </button>
-)
-
-export default SaveTripButton;

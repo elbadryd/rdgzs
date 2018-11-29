80d4ac14516f5ea1e062b261bcff9e54fbb67dc0
J Patrick <jpatricknola@gmail.com>
2018-11-29 10:48:57 -0600

    updated server

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,14 +2,14 @@ const { loginRouter } = require('./login');
 const { signupRouter } = require('./signup');
 const { createRoute } = require('./createRoute');
 const { redrawRouter } = require('./redraw');
-const { addStopRouter } = require('./addStop');
-const { addTripRouter } = require('./addTrip');
+const { stopRouter } = require('./addStop');
+const { tripRouter } = require('./addTrip');
 
 module.exports = (app) => {
   app.use('/login', loginRouter);
   app.use('/signup', signupRouter);
   app.use('/createRoute', createRoute);
   app.use('/redraw', redrawRouter);
-  app.use('/stop', addStopRouter);
-  app.use('/trip', addTripRouter);
+  app.use('/stop', stopRouter);
+  app.use('/trip', tripRouter);
 };

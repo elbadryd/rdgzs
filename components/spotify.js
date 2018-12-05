b69e1330ab53852f69a23ddfe8469933238bdbdb
Dario Elbadry <elbadry.d@gmail.com>
2018-12-05 12:29:34 -0600

    pullingSpotify

--- components/spotify.js
+++ components/spotify.js
@@ -1,5 +1,6 @@
 import { connect } from "react-redux";
 import axios from 'axios';
+import wdk from 'wikidata-sdk';
 const turf = require('@turf/helpers');
 const along = require('@turf/along');
 const distance = require('@turf/distance');
@@ -11,6 +12,7 @@ class Spotify extends React.Component {
       }
       this.getCities = this.getCities.bind(this);
       this.getQueryPoints = this.getQueryPoints.bind(this);
+      this.getEntities = this.getEntities.bind(this);
     }   
   
   
@@ -25,7 +27,8 @@ class Spotify extends React.Component {
       console.log(response);
       let top = [];
       response.forEach((result, i)=>{
-        let highest = result.data.records.slice(2);
+        let highest = result.data.records.slice(0, 3);
+        console.log(highest);
         highest.forEach(record=>{
           if (!top[i]){
             top[i] = [`${record.fields.city}, ${record.fields.state}`];
@@ -52,18 +55,33 @@ class Spotify extends React.Component {
     console.log(dist);
     console.log(polyline);
     let points = line.map((coords, i) => {
-      if (dist > (i * 200) + 1){
-        return along.default(polyline, i * 200)
+      if (dist > (i * 150) + 1){
+        return along.default(polyline, i * 150)
       }
     })
     let coords = points.filter(coord =>{
       return coord !== undefined;
     })
+    console.log(coords);
     this.getCities(coords);
+  }
+  getEntities(){
+    let city = "Monroe, Louisiana"
+    let sparql = `SELECT DISTINCT ?item WHERE { ?item (wdt: P31/wdt:P279*) wd:Q515. ?item ?label "Lafayette, Louisiana"@en.}`
+    axios.get(`https://query.wikidata.org/sparql?query=${sparql}&format=JSON`)
+    .then(response=>{
+      console.log(response);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
   }
   render(){
     return(
+      <div>
       <div><img onClick={this.getQueryPoints} src="/static/spotify.png"></img></div>
+      <div onClick={this.getEntities}>getentities</div>
+      </div>
     )
   }
 }

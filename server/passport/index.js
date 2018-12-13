94626c1368311eb2cf0c15a965f56ea2b8fa1ea1
Randy Calcaterra <opalibis64@gmail.com>
2018-12-13 15:10:26 -0600

    working on getting deploy vars set, spotify callbackURL

--- server/passport/index.js
+++ server/passport/index.js
@@ -72,7 +72,7 @@ module.exports = (app) => {
     {
       clientID: process.env.SPOTIFY_ID,
       clientSecret: process.env.SPOTIFY_SECRET,
-      callbackURL: 'http://localhost:3000/login/callback',
+      callbackURL: 'http://localhost:3000/login/callback' || 'http://54.211.45.243:3000/login/callback',
       passReqToCallback: true,
     },
     ((req, accessToken, refreshToken, expires_in, profile, done) => {

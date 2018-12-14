e00147cd7b990543df9bee4221a05c171ffb056a
Randy Calcaterra <opalibis64@gmail.com>
2018-12-13 19:09:40 -0600

    added helmet and set env vars for spotify callback uri

--- server/passport/index.js
+++ server/passport/index.js
@@ -72,7 +72,7 @@ module.exports = (app) => {
     {
       clientID: process.env.SPOTIFY_ID,
       clientSecret: process.env.SPOTIFY_SECRET,
-      callbackURL: 'http://54.211.45.243:3000/login/callback',
+      callbackURL: process.env.CALLBACK_URL,
       passReqToCallback: true,
     },
     ((req, accessToken, refreshToken, expires_in, profile, done) => {
